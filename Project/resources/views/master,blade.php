c4fec6fd878d8ceb78dc7c23f90dda489fa359cb
Mita57 <57thrasher57@gmail.com>
2020-12-15 20:08:42 +0500

    Adding blade templates

---
+++ Project/resources/views/master,blade.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>{{ page_title }}</title>
+        <link type="text/css" rel="stylesheet" href="{{ style }}">
+    </head>
+    <body>
+        <div id="header">
+        </div>
+        @yield('content')
+    </body>
+</html>
