a8fe169bb7ba128f092c07a712a95f1c91166d0b
Mita57 <57thrasher57@gmail.com>
2021-01-14 21:45:40 +0500

    Adding laravel passport

--- Project/app/User.php
+++ Project/app/User.php
@@ -5,9 +5,11 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Authenticatable {
-    use Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $table = 'Users';
 
