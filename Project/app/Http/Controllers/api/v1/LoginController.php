c81df3612e2ba9ee3c389a75c3caa5d4dc87a26c
Mita57 <57thrasher57@gmail.com>
2021-01-19 20:34:47 +0500

    Doing a lot of stuff

---
+++ Project/app/Http/Controllers/api/v1/LoginController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name'=>'required|max:55',
+            'email'=>'email|required|unique:users',
+            'password'=>'required|confirmed'
+        ]);
+
+        $validatedData['password'] = bcrypt($request->password);
+
+        $user = User::create($validatedData);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response(['user'=> $user, 'access_token'=> $accessToken]);
+
+    }
+
+
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if(!auth()->attempt($loginData)) {
+            return response(['message'=>'Hey buddy I think you got the wrong door']);
+        }
+
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+
+    }
+}
